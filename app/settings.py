079374f3dc7603aea004aa191072aa43a83d271a
daniel_gomes <danielcerqueira7747@gmail.com>
2025-01-02 21:32:07 -0300

    feat: EMAIL_HOST_PASSWORD as enviroment variable

--- app/settings.py
+++ app/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'suporteconstsoft@gmail.com'
-EMAIL_HOST_PASSWORD = 'igmf xosf tzwk eame'
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
 
