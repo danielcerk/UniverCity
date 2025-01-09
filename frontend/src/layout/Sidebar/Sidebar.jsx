c8e5ead3e7072a8d694d5cbb7d1cfac03d451fa4
Thyago <thiagogg020676@gmail.com>
2025-01-09 01:14:59 -0300

    add css in sidebar

--- frontend/src/layout/Sidebar/Sidebar.jsx
+++ frontend/src/layout/Sidebar/Sidebar.jsx
@@ -21,28 +21,56 @@ export default function Sidebar() {
         <> {/* Envolvendo o conteúdo com um div */}
             {isAuth ? (
                 <Col xs={12} md={3} className="p-3">
-                    <div className="d-flex flex-column">
-                        <Link
-                            to="/feed"
-                            className="text-left d-flex align-items-center text-decoration-none text-dark mb-2"
-                        >
-                            <FaHome className="me-2" /> Home
-                        </Link>
-                        <Link
-                            to="/conta"
-                            className="text-left d-flex align-items-center text-decoration-none text-dark mb-2"
-                        >
-                            <FaUserAlt className="me-2" /> Conta
-                        </Link>
-                        <Link
-                            to="/comunidades"
-                            className="text-left d-flex align-items-center text-decoration-none text-dark"
-                        >
-                            <FaUsers className="me-2" /> Universidades
-                        </Link>
+    
+                <div id="nav-bar" className="bg-dark" style={{marginTop:'100px'}}>
+                <input type="checkbox" id="nav-toggle"></input>
+                <div id="nav-header">
+                    <a id="nav-title" target="_blank">
+                        Explore
+                    </a>
+
+                    <label htmlFor="nav-toggle">
+                   
+                    <span id="" className="fa-solid fa-ellipsis"></span>
+                    </label>
+                    <hr></hr>
+                </div>
+                <div id="nav-content">
+                    <div className="nav-button">
+                    <Link
+                        to="/feed"
+                        className="text-left d-flex align-items-center text-decoration-none text-white mb-2 nav-button"
+                    >
+                        <FaHome className="me-2" /> <span>Home</span>
+                    </Link>
                     </div>
+                    <div className="nav-button">
+                    <Link
+                        to="/conta"
+                        className="text-left d-flex align-items-center text-decoration-none text-white mb-2 nav-button"
+                    >
+                        <FaUserAlt className="me-2" /> <span>Conta</span>
+                    </Link>
+                    </div>
+                    <div className="nav-button">
+                    <Link
+                        to="/comunidades"
+                        className="text-left d-flex align-items-center text-decoration-none text-white nav-button"
+                    >
+                        <FaUsers className="me-2" /> <span>Universidades</span>
+                    </Link>
+                    </div>
+                 
+                </div>
+
+                </div>
+
                 </Col>
+
+                
             ) : null}  {/* Não renderiza nada quando isAuth é false */}
+
+            
         </>
     );
 }
